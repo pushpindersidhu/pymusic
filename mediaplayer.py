fd2576fc9d601e860cdf6b19c84473cbc6c10233
Sidhu <pushpindersidhu1984@gmail.com>
2022-01-08 14:06:10 +0530

    Added event manger to MediaPlayer

--- mediaplayer.py
+++ mediaplayer.py
@@ -4,26 +4,103 @@ from playback_status import PlaybackStatus
 
 class MediaPlayer:
 
+    onMediaPlayerTimeChanged = None
+    onMediaPlayerPositionChanged = None
+    onMediaDurationChanged = None
+    onMediaStateChanged = None
+    onMediaPlayerEndReached = None
+    onMediaPlayerMediaChanged = None
+    onMediaPlayerPaused = None
+    onMediaPlayerPlaying = None
+    onMediaPlayerMuted = None
+    onMediaPlayerUnmuted = None
+    onMediaPlayerStopped = None
+    onMediaPlayerPausableChanged = None
+
     def __init__(self) -> None:
         self.instance = vlc.Instance()
-        self.mediaplayer = self.instance.media_player_new()
+        self.mediaPlayer = self.instance.media_player_new()
+        self.eventManager = self.mediaPlayer.event_manager()
         self.media = None
         self.playbackstatus = PlaybackStatus.STOPPED
 
-    def set_media(self, mrl):
+    def setMedia(self, mrl):
         self.media = self.instance.media_new(mrl)
-        self.mediaplayer.set_media(self.media)
+        self.mediaPlayer.set_media(self.media)
 
     def release(self):
         self.media.release()
-        self.mediaplayer.release()
+        self.mediaPlayer.release()
+
+    @vlc.callbackmethod
+    def onMediaPlayerTimeChangedCallback(self, event):
+        if self.onMediaPlayerTimeChanged is not None:
+            self.onMediaPlayerTimeChanged()
+
+    def setOnMediaPlayerTimeChangedCallback(self, callback):
+        self.eventManager.event_attach(vlc.EventType.MediaPlayerTimeChanged,
+                                       self.onMediaPlayerTimeChangedCallback)
+        self.onMediaPlayerTimeChanged = callback
+
+    @vlc.callbackmethod
+    def onMediaPlayerPositionChangedCallback(self, event):
+        if self.onMediaPlayerPositionChanged is not None:
+            self.onMediaPlayerPositionChanged()
+
+    def setOnMediaPlayerPositionChangedCallback(self, callback):
+        self.eventManager.event_attach(vlc.EventType.MediaPlayerPositionChanged,
+                                       self.onMediaPlayerPositionChangedCallback)
+        self.onMediaPlayerPositionChanged = callback
+
+    @vlc.callbackmethod
+    def onMediaDurationChangedCallback(self, event):
+        if self.onMediaDurationChanged is not None:
+            self.onMediaDurationChanged()
+
+    def setOnMediaDurationChangedCallback(self, callback):
+        self.eventManager.event_attach(vlc.EventType.MediaDurationChanged,
+                                       self.onMediaDurationChangedCallback)
+        self.onMediaDurationChanged = callback
+
+    @vlc.callbackmethod
+    def onMediaStateChangedCallback(self, event):
+        if self.onMediaStateChanged is not None:
+            self.onMediaStateChanged()
+
+    def setOnMediaStateChangedCallback(self, callback):
+        self.eventManager.event_attach(vlc.EventType.MediaStateChanged,
+                                       self.onMediaStateChangedCallback)
+        self.onMediaStateChanged = callback
+
+    @vlc.callbackmethod
+    def onMediaPlayerEndReachedCallback(self, event):
+        if self.onMediaPlayerEndReached is not None:
+            self.onMediaPlayerEndReached()
+
+    def setOnMediaPlayerEndReachedCallback(self, callback):
+        self.eventManager.event_attach(vlc.EventType.MediaPlayerEndReached,
+                                       self.onMediaPlayerEndReachedCallback)
+        self.onMediaPlayerEndReached = callback
+
+    @vlc.callbackmethod
+    def onMediaPlayerMediaChangedCallback(self, event):
+        if self.onMediaPlayerMediaChanged is not None:
+            self.onMediaPlayerMediaChanged()
+
+    def setOnMediaPlayerMediaChangedCallback(self, callback):
+        self.eventManager.event_attach(vlc.EventType.MediaPlayerPositionChanged,
+                                       self.onMediaPlayerMediaChangedCallback)
+        self.onMediaPlayerMediaChanged = callback
+
+    def attachEvent(self, event_type, callback):
+        self.eventManager.event_attach(event_type, callback)
 
     def play(self):
-        self.mediaplayer.play()
+        self.mediaPlayer.play()
         self.playbackstatus = PlaybackStatus.PLAYING
 
     def stop(self):
-        self.mediaplayer.stop()
+        self.mediaPlayer.stop()
         self.playbackstatus = PlaybackStatus.STOPPED
 
     def restart(self):
@@ -31,50 +108,57 @@ class MediaPlayer:
         self.play()
 
     def pause(self):
-        self.mediaplayer.pause()
+        self.mediaPlayer.pause()
         self.playbackstatus = PlaybackStatus.PAUSED
 
     def unpause(self):
-        self.mediaplayer.pause()
+        self.mediaPlayer.pause()
         self.playbackstatus = PlaybackStatus.PLAYING
 
-    def get_length(self):
-        self.mediaplayer.get_length()
+    def getLength(self):
+        self.mediaPlayer.get_length()
 
-    def set_volume(self, volume):
-        return self.mediaplayer.audio_set_volume(volume)
+    def getState(self):
+        self.mediaPlayer.get_state()
 
-    def get_volume(self):
-        return self.mediaplayer.audio_get_volume()
+    def setVolume(self, volume):
+        return self.mediaPlayer.audio_set_volume(volume)
 
-    def get_playbackstatus(self):
+    def getVolume(self):
+        return self.mediaPlayer.audio_get_volume()
+
+    def getPlaybackStatus(self):
         return self.playbackstatus
 
-    def is_playing(self):
+    def isPlaying(self):
         return self.playbackstatus == PlaybackStatus.PLAYING
 
-    def set_position(self, position):
-        self.mediaplayer.set_position(position)
+    def setPosition(self, position):
+        self.mediaPlayer.set_position(position)
 
-    def get_position(self):
-        return self.mediaplayer.get_position()
+    def getPosition(self):
+        return self.mediaPlayer.get_position()
 
-    def get_time(self):
-        return self.mediaplayer.get_time()
+    def getTime(self):
+        return self.mediaPlayer.get_time()
 
-    def set_time(self, time_ms):
-        self.mediaplayer.set_time(time_ms)
+    def setTime(self, time_ms):
+        self.mediaPlayer.set_time(time_ms)
 
 
 if __name__ == '__main__':
     import time
 
+    def onTimeChanged():
+        print(mediaPlayer.getPosition())
+
     mediaPlayer = MediaPlayer()
-    mediaPlayer.set_media('/home/sidhu/Music/Sidhu/10. The Weeknd - Tears In The Rain.flac')
-    mediaPlayer.set_volume(10)
-    print("Volume: ", mediaPlayer.get_volume())
+    mediaPlayer.setOnMediaPlayerPositionChangedCallback(onTimeChanged)
+    mediaPlayer.setMedia('/home/sidhu/Music/Sidhu/10. The Weeknd - Tears In The Rain.flac')
+    mediaPlayer.setVolume(10)
+    print("Volume: ", mediaPlayer.getVolume())
     mediaPlayer.play()
-    mediaPlayer.set_position(0.50)
+    mediaPlayer.setPosition(0.50)
     print('Playing at length: 50%')
     time.sleep(1)
     mediaPlayer.pause()
@@ -86,12 +170,12 @@ if __name__ == '__main__':
     print('Restarting...')
     mediaPlayer.restart()
     print('Playing at time: 100s')
-    mediaPlayer.set_time(50)
+    mediaPlayer.setTime(50)
     print('Setting volume: 1')
-    mediaPlayer.set_volume(0)
+    mediaPlayer.setVolume(0)
     time.sleep(1)
-    print(mediaPlayer.get_volume())
-    print(mediaPlayer.get_length())
-    print(mediaPlayer.get_playbackstatus())
-    print(mediaPlayer.get_position())
-    print(mediaPlayer.is_playing())
+    print(mediaPlayer.getVolume())
+    print(mediaPlayer.getLength())
+    print(mediaPlayer.getPlaybackStatus())
+    print(mediaPlayer.getPosition())
+    print(mediaPlayer.isPlaying())
---
+++ mediaplayer_state.py
@@ -0,0 +1,12 @@
+import enum
+from vlc import State
+
+class MediaPlayerState(enum.Enum):
+    NothingSpecial = State.NothingSpecial
+    Opening = State.Opening
+    Buffering = State.Buffering
+    Playing = State.Playing
+    Paused = State.Paused
+    Stopped = State.Stopped
+    Ended = State.Ended
+    Error = State.Error
