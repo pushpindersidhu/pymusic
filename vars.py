f906ae1a2b4d24d4fe95c8193f7e44a1fb343584
Sidhu <pushpindersidhu1984@gmail.com>
2022-01-12 16:00:56 +0530

    Artist view changes

--- vars.py
+++ vars.py
@@ -2,7 +2,7 @@ import os
 
 SUPPORTED_EXTENSIONS = ['.mp3', '.m4a', '.flac']
 
-DIRS = ['/home/sidhu/Music/Sidhu']
+DIRS = ['/home/sidhu/Music/music']
 
 LIBRARY_PATH = '/home/sidhu/Music/Library'
 
